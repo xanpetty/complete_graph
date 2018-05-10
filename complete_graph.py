41414b7653adb3d6be89ec93c63ed6f51ba7fb81
xanpetty <38663842+xanpetty@users.noreply.github.com>
2018-05-10 12:38:41 -0400

    Create complete_graph.py

---
+++ complete_graph.py
@@ -0,0 +1,13 @@
+import sys
+
+def complete_graph(nodes):
+
+  connections = ( (nodes * (nodes-1)) / 2 )
+  return(int(connections))
+
+
+nodes = int(sys.argv[1])
+
+connections = complete_graph(nodes)
+
+print("number of nodes: " + str(nodes) + "\tnumber of connections " + str(connections))
